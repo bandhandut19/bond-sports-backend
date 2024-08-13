5229eb0a50069dd453a976f8a5b62399f8df951e
bandhandut19 <bondon21081@gmail.com>
2024-08-14 02:39:57 +0600

    changed origin to vercel

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ const app: Application = express()
 app.use(express.json())
 app.use(
   cors({
-    // origin: 'https://bond-sports-frontend.vercel.app',
-    origin: 'http://localhost:5173',
+    origin: 'https://bond-sports-frontend.vercel.app',
+    // origin: 'http://localhost:5173',
     credentials: true,
   }),
 )

0e855459585ee092c85c2417175f6bffeea37371
bandhandut19 <bondon21081@gmail.com>
2024-12-10 11:21:04 +0600

    created admin in database

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import app from './app'
 import config from './app/config'
 import mongoose from 'mongoose'
+import { seedAdmin } from './app/utils/seedAdmin'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-
+    seedAdmin()
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`)
     })
